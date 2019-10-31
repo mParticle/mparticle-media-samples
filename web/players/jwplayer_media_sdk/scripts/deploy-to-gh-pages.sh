56764c549a88a7dd7eb5b6a1f5b6a1100ea754bf
Alexander Sapountzis <alexs@mparticle.com>
2019-10-31 14:19:26 -0400

    Remove comments

--- web/players/jwplayer_media_sdk/scripts/deploy-to-gh-pages.sh
+++ web/players/jwplayer_media_sdk/scripts/deploy-to-gh-pages.sh
@@ -27,6 +27,4 @@ echo "Deploying to Github Pages"
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-echo "${GITHUB_REPO}"
-echo "${GITHUB_TOKEN}"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
